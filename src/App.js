84028026a13d9e0c1a39313fb2ed8dc656bc2cf5
amaan1203 <127188069+amaan1203@users.noreply.github.com>
2023-12-16 03:20:27 +0530

    added footer and schedule section

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./components/landingpage";
 import Footer from "./components/Footer";
+import Schedule from "./components/Schedule";
+
 function App() {
   return (
-    <div> <Router>
+    <div className="flex flex-col items-center"> <Router>
       <Routes>
         <Route exact path="/" element={<LandingPage />} />
       </Routes>
+      <Schedule></Schedule>
       <Footer>
       </Footer>
     </Router>
